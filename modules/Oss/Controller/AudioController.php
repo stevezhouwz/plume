7f1c79e8f05043eb2359e9f3b74b9c75511edd1c
zhoudw <zhoudw>
2019-05-30 10:27:56 +0800

    测试

---
+++ modules/Oss/Controller/AudioController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Oss\Controller;
+
+use AlibabaCloud\Client\AlibabaCloud;
+use AlibabaCloud\Client\Exception\ClientException;
+use AlibabaCloud\Client\Exception\ServerException;
+use OSS\Core\OssException;
+use OSS\OssClient;
+use Oss\Utils\AipSpeech;
+use Plume\Core\Controller;
+use Oss\Utils\Ossupload;
+
+
+
+class AudioController extends Controller
+{
+
+    // 请求参数key
+     const KEY_APP_KEY = "KwRKmbtdBB0JM9r3";
+     const KEY_FILE_LINK = "file_link";
+     const KEY_VERSION = "version";
+     const KEY_ENABLE_WORDS = "enable_words";
+    // 响应参数key
+     const KEY_TASK_ID = "TaskId";
+     const KEY_STATUS_TEXT = "StatusText";
+     const KEY_RESULT = "Result";
+    // 状态值
+     const STATUS_SUCCESS = "SUCCESS";
+     const STATUS_RUNNING = "RUNNING";
+     const STATUS_QUEUEING = "QUEUEING";
+
+
+    function submitFileTransRequest($appKey, $fileLink) {
+        // 获取task json字符串，包含appkey和file_link参数等；
+        // 新接入请使用4.0版本，已接入(默认2.0)如需维持现状，请注释掉该参数设置
+        // 设置是否输出词信息，默认为false，开启时需要设置version为4.0
+        $taskArr = array(self::KEY_APP_KEY => $appKey, self::KEY_FILE_LINK => $fileLink, self::KEY_VERSION => "4.0", self::KEY_ENABLE_WORDS => FALSE);
+        $task = json_encode($taskArr);
+        print $task . "\n";
+        try {
+            // 提交请求，返回服务端的响应
+            $submitTaskResponse = AlibabaCloud::nlsFiletrans()
+                ->v20180817()
+                ->submitTask()
+                ->withTask($task)
+                ->request();
+            print $submitTaskResponse . "\n";
+            // 获取录音文件识别请求任务的ID，以供识别结果查询使用
+            $taskId = NULL;
+            $statusText = $submitTaskResponse[self::KEY_STATUS_TEXT];
+            if (strcmp(self::STATUS_SUCCESS, $statusText) == 0) {
+                $taskId = $submitTaskResponse[self::KEY_TASK_ID];
+            }
+            return $taskId;
+        } catch (ClientException $exception) {
+            // 获取错误消息
+            print_r($exception->getErrorMessage());
+        } catch (ServerException $exception) {
+            // 获取错误消息
+            print_r($exception->getErrorMessage());
+        }
+    }
+
+
+    function getFileTransResult($taskId) {
+        $result = NULL;
+        while (TRUE) {
+            try {
+
+                $getResultResponse = AlibabaCloud::nlsFiletrans()
+                    ->v20180817()
+                    ->getTaskResult()
+                    ->withTaskId($taskId)
+                    ->request();
+                print "识别查询结果: " . $getResultResponse . "\n";
+                $statusText = $getResultResponse[self::KEY_STATUS_TEXT];
+                if (strcmp(self::STATUS_RUNNING, $statusText) == 0 || strcmp(self::STATUS_QUEUEING, $statusText) == 0) {
+                    // 继续轮询
+                    sleep(3);
+                }
+                else {
+                    if (strcmp(self::STATUS_SUCCESS, $statusText) == 0) {
+                        $result = $getResultResponse;
+                    }
+                    // 退出轮询
+                    break;
+                }
+            } catch (ClientException $exception) {
+                // 获取错误消息
+                print_r($exception->getErrorMessage());
+            } catch (ServerException $exception) {
+                // 获取错误消息
+                print_r($exception->getErrorMessage());
+            }
+        }
+        return $result;
+    }
+
+    public function indexAction(){
+        $this->api();
+        $accessKeyId = "LTAIQqL13WHU9JkA";
+        $accessKeySecret = "Iiqj2kANqCY9lo7QGAZb4Emp0vWKfB";
+        $appKey = self::KEY_APP_KEY;
+        $fileLink = "http://nhds.oss-cn-hangzhou.aliyuncs.com/picture-wall/audio/kaimushi/test_94044909.wav";
+
+        AlibabaCloud::accessKeyClient($accessKeyId, $accessKeySecret)
+            ->regionId("cn-shanghai")
+            ->asGlobalClient();
+
+
+        $taskId = $this->submitFileTransRequest($appKey, $fileLink);
+        if($taskId){
+            print "录音文件识别请求成功，task_id: " . $taskId . "\n";
+            $result = $this->getFileTransResult($taskId);
+            print_r($result);die;
+        }
+        die;
+    }
+
+
+}
